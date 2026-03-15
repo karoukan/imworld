476739f2c24723eb211d67309e6b39cfba3cd20c
Processor Dumblecore <>
2026-03-15 13:55:46 +0100

    fight loop iteration

--- war.go
+++ war.go
@@ -21,36 +21,95 @@ func war(s *Sector, attacker int) {
 
 			for defender := 0; defender < len(s.Factions); defender++ {
 				if attacker != defender && s.Factions[defender].War == false {
-					if s.Factions[attacker].Type == "enterprise" {
+					switch s.Factions[attacker].Type {
+					case "enterprise":
 						if s.Factions[defender].Resources.Credits > 5 {
 							s.Factions[defender].War = true
 							fmt.Println(s.Factions[defender].Name, "vs", s.Factions[attacker].Name)
 
 							for s.Factions[attacker].War == true && s.Factions[defender].War == true {
-								resourcesSteal := 0
-								for s.Factions[defender].Resources.Credits >= 5 {
-									// fmt.Println("RESOURCES(", s.Factions[i].Name, ") BEFORE STEAL", s.Factions[i].Resources)
-									// fmt.Println("RESOURCES(", s.Factions[defender].Name, ") BEFORE STEAL", s.Factions[defender].Resources)
-									resourcesSteal = s.Factions[defender].Resources.Credits / 10
-
-									if resourcesSteal == 0 {
-										resourcesSteal = 1
-										s.Factions[defender].War = false
-										s.Factions[attacker].War = false
-										endwar(s, attacker)
-									}
-
-									s.Factions[defender].Resources.Credits -= resourcesSteal
-									s.Factions[attacker].Resources.Credits += resourcesSteal
-									// fmt.Println("RESOURCES(", s.Factions[defender].Name, ") AFTER STEAL", s.Factions[defender].Resources)
-									// fmt.Println("RESOURCES(", s.Factions[i].Name, ") AFTER STEAL", s.Factions[i].Resources)
+								a, b := steal(s.Factions[attacker].Resources.Credits, s.Factions[defender].Resources.Credits)
+								if b == 0 {
+									s.Factions[attacker].War = false
+									s.Factions[defender].War = false
+									endwar(s, attacker)
 								}
+								s.Factions[attacker].Resources.Credits = a
+								s.Factions[defender].Resources.Credits = b
+								fmt.Println(a, b)
+
+							}
+						}
+					case "collectif":
+						if s.Factions[defender].Resources.Data > 5 {
+							s.Factions[defender].War = true
+							fmt.Println(s.Factions[defender].Name, "vs", s.Factions[attacker].Name)
+
+							for s.Factions[attacker].War == true && s.Factions[defender].War == true {
+								a, b := steal(s.Factions[attacker].Resources.Data, s.Factions[defender].Resources.Data)
+								if b == 0 {
+									s.Factions[attacker].War = false
+									s.Factions[defender].War = false
+									endwar(s, attacker)
+								}
+								s.Factions[attacker].Resources.Data = a
+								s.Factions[defender].Resources.Data = b
+								fmt.Println(a, b)
+
+							}
+						}
+					case "mafia":
+						if s.Factions[defender].Resources.Credits > 5 {
+							s.Factions[defender].War = true
+							fmt.Println(s.Factions[defender].Name, "vs", s.Factions[attacker].Name)
+
+							for s.Factions[attacker].War == true && s.Factions[defender].War == true {
+								a, b := steal(s.Factions[attacker].Resources.Credits, s.Factions[defender].Resources.Credits)
+								if b == 0 {
+									s.Factions[attacker].War = false
+									s.Factions[defender].War = false
+									endwar(s, attacker)
+								}
+								s.Factions[attacker].Resources.Credits = a
+								s.Factions[defender].Resources.Credits = b
+								fmt.Println(a, b)
+
+							}
+						}
+					case "classified":
+						if s.Factions[defender].Resources.Credits > 5 {
+							s.Factions[defender].War = true
+							fmt.Println(s.Factions[defender].Name, "vs", s.Factions[attacker].Name)
+
+							for s.Factions[attacker].War == true && s.Factions[defender].War == true {
+								a, b := steal(s.Factions[attacker].Resources.Credits, s.Factions[defender].Resources.Credits)
+								if b == 0 {
+									s.Factions[attacker].War = false
+									s.Factions[defender].War = false
+									endwar(s, attacker)
+								}
+								s.Factions[attacker].Resources.Credits = a
+								s.Factions[defender].Resources.Credits = b
+								fmt.Println(a, b)
+
 							}
 						}
 					}
 				}
 			}
-
+			s.Factions[attacker].War = false
 		}
 	}
+}
+
+func steal(attRes int, defRes int) (int, int) { // Steal resource during ATTACK
+	amount := defRes / 10
+
+	if amount == 0 {
+		amount = 1
+	}
+
+	newDefRes := defRes - amount
+	newAttRes := attRes + amount
+	return newAttRes, newDefRes
 }
